95a50d122a2a65b69372d5589cb78e28e7bab7e6
ara1143 <aramahjabeen114@gmail.com>
2025-10-02 21:00:23 -0400

    Initial commit

---
+++ src/main/java/com/card/personal_card/PersonalCardApplication.java
@@ -0,0 +1,13 @@
+package com.card.personal_card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalCardApplication.class, args);
+	}
+
+}
